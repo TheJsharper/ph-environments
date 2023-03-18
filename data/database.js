b3d1e6421448d640f4c672613f46a487d555089c
Pedro Salomon <pedro.salomon@gmail.com>
2023-03-18 11:04:49 +0100

    cluster address added

--- data/database.js
+++ data/database.js
@@ -8,6 +8,7 @@ const dbName = process.env.MONGODB_DB_NAME;
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
+
 console.log('Trying to connect to db');
 
 try {
